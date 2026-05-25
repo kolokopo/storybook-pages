6dba1ff943a82a4473cdedb037bb2d47a45b32fe
kolokopo <mafifulamin@gmail.com>
2026-05-25 21:49:38 +0700

    Deploy multiple branch

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,30 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../src/components/**/*.stories.@(js|jsx)'],
+
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
+    "@storybook/addon-a11y",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
   docs: {
     autodocs: "tag",
   },
+
+  viteFinal: async (config) => {
+    // IMPORTANT: static base for GitHub Pages project site
+    config.base = "/storybook-pages/";
+    return config;
+  },
 };
+
 export default config;
