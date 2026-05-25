c16b9c43bc2eb3ce5072049657b2f5e170074719
kolokopo <mafifulamin@gmail.com>
2026-05-25 21:38:57 +0700

    Change base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://01clarian.github.io/storybook-example",
+  base:"https://kolokopo.github.io/storybook-pages/",
   plugins: [react()],
   build: {
     rollupOptions: {
